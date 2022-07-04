c72cd455e244fb65824c2290dcea401288f5f104
Vlex <98803111+Vlxkvi@users.noreply.github.com>
2022-07-05 01:42:29 +0300

    Добавьте файлы проекта.

---
+++ ApiSpotify/Controllers/MySpotifyController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpotifyAPI.Web;
+using TGbot.Constant;
+
+namespace ApiSpotify.Controllers
+{
+    
+    [ApiController]
+    [Route("[controller]/[action]")]
+    
+    public class SpotifyController : ControllerBase
+    {
+        [HttpGet(Name = "PlaylistTracksID")]
+        public async Task<Dictionary<int, string>> GetPlaylistTracksID(string PlaylistID)
+        {
+            var s_id = new Dictionary<int, string>();
+
+            var spotify = new SpotifyClient(constants.token);
+            var playlist = await spotify.Playlists.Get(PlaylistID);
+
+            int i = 0;
+            foreach (PlaylistTrack<IPlayableItem> item in playlist.Tracks.Items)
+            {
+                if (item.Track is FullTrack track)
+                {
+                    i++;
+                    s_id[i] = track.Id;
+                }
+                if (item.Track is FullEpisode episode)
+                {
+                    i++;
+                    s_id[i] = episode.Id;
+                }
+            }
+            return s_id;
+        }
+
+
+
+        [HttpGet(Name = "PlaylistTracksNames")]
+        public async Task<Dictionary<int, string>> GetPlaylistTracksNames(string PlaylistID)
+        {
+            var s_names = new Dictionary<int, string>();
+
+            var spotify = new SpotifyClient(constants.token);
+            var playlist = await spotify.Playlists.Get(PlaylistID);
+
+            int i = 0;
+            foreach (PlaylistTrack<IPlayableItem> item in playlist.Tracks.Items)
+            {
+                if (item.Track is FullTrack track)
+                {
+                    i++;
+                    s_names[i] = track.Name;
+                }
+                if (item.Track is FullEpisode episode)
+                {
+                    i++;
+                    s_names[i] = episode.Name;
+                }
+            }
+            return s_names;
+        }
+
+        [HttpPost(Name = "CreatePlaylist")]
+        public async Task<string> CreatePlaylist(string userID, string Name)
+        {
+            var spotify = new SpotifyClient(constants.token);
+            var request = new PlaylistCreateRequest(Name);
+            var playlist = await spotify.Playlists.Create(userID, request);
+
+            return playlist.Id;
+        }
+
+        [HttpPut(Name = "AddToPlaylist")]
+        public async Task<string> AddtoPlaylist(string PlaylistID, string TracksID)
+        {
+            string[] ids = TracksID.Split("$$");
+            IList<string> names = new List<string>();
+            foreach (var a in ids)
+            {
+                names.Add($"spotify:track:{a}");
+            }
+
+            var spotify = new SpotifyClient(constants.token);
+            var request = new PlaylistAddItemsRequest(names);
+            await spotify.Playlists.AddItems(PlaylistID, request);
+            return PlaylistID;
+        }
+    }
+}
---
+++ ApiSpotify/constant/constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TGbot.Constant
+{
+    class constants
+    {
+        public static int limit = 3;
+        public static string token = "BQD26K86RNtv7nAFvbk2s8Jlb9gsr0iRINYQZah7Hdho_If1JwT1vmP-Wf0JiVvULn3jkR_YEkqNYXu76qYTk9a25wtMSsLkyr7PKKy95w6W0lEgMo9cw5ngBr8IDwd0zI3g3rTWTa_0kLlsOi1DuAevnhcPIA_YtecgFAas9I3v1IogZDiZsnDbyFI-CTTTEWEzxVD6JM8nmqPBcJiHTMTbWnCED51MCOSJYyu5dSR_nbuD0KCDBG32W49w";
+    }
+}
